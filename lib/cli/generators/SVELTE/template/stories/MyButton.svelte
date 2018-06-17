9f36d78fccccd1f3ef52579cb0ba24363599c8bc
Gavin King <gavin.king@itslearning.com>
2018-06-17 16:43:13 +0200

    Svelte generators added, not tested

---
+++ lib/cli/generators/SVELTE/template/stories/MyButton.svelte
@@ -0,0 +1,29 @@
+<button on:click="onClick(event)">
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    border: 1px solid #eee;
+    border-radius: 3;
+    background-color: #FFFFFF;
+    cursor: pointer;
+    font-size: 15;
+    padding: 3px 10px;
+    margin: 10;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {};
+    },
+
+    methods: {
+      onClick() {
+        this.fire('click', event);
+      },
+    },
+  };
+</script>
